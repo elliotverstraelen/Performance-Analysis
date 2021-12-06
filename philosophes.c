57f70f4553569c39469ac21c2d7b78131db21734
Elliot Verstraelen <elliot.verstraelen@telenet.be>
2021-12-06 15:52:09 +0100

    corrections philosophes.c

--- philosophes.c
+++ philosophes.c
@@ -2,31 +2,29 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
-#include<semaphore.h>
 #include <string.h>
-
-
-void * philosophe(void *);
-void mange(int);
-void error(int, char *);
-void prend_baguettes(int id);
-void relache_baguettes(int id);
+#include <ctype.h>
 
 typedef struct philosophe{
     int is_pair;
-    pthread_mutex_t *right;
-    pthread_mutex_t left;
+    pthread_mutex_t *left;
+    pthread_mutex_t right;
 } philosophe_t;
+
+int main(int argc, char *argv[]);
+void * philosophe(void * arg);
+void mange();
+void prend_baguettes(philosophe_t *id);
+void relache_baguettes(philosophe_t *id);
+
 
 void *philosophe (void* arg)
 {
     philosophe_t *id = (philosophe_t *) arg;
-    printf("\n le philosophe %d pense", *id);
     for(int i=0; i<100000; i++){
-        prend_baguettes(*id);
-        mange(*id);
-        relache_baguettes(*id);
+        prend_baguettes(id);
+        mange(id);
+        relache_baguettes(id);
     }
     
 
@@ -47,18 +45,12 @@ void prend_baguettes(philosophe_t *id){
 void relache_baguettes(philosophe_t *id)
 {
     pthread_mutex_unlock(id->left); //relache la baguette de gauche
-    pthread_mutex_unlock(&p->right); // relache la baguette de droite
+    pthread_mutex_unlock(&id->right); // relache la baguette de droite
 }
 
 void mange()
 {
     return;
-}
-
-void error(int err, char *msg)
-{
-    pfrintf(stderr, "%s a retourné %d message d'erreur: %s\n", msg, err, strerror(errno));
-    exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[])
@@ -68,18 +60,18 @@ int main(int argc, char *argv[])
         // autre poss. : lance le programme avec des valeurs par defaut
         printf("Arguments inssufisants. Veuillez préciser un nombre de readers et writers.\n ./reader_writer -r <readers> -w <writers>");
     }
-    else if((argv[1]!="-p") || !(argv[2].isdigit()) || (argv[1]!="--philosophes")){
+    else if((argv[1]!="-p") || !isdigit(argv[2]) || (argv[1]!="--philosophes")){
         printf("Erreur dans les arguments. Veuillez préciser un nombre de readers et writers.\n ./reader_writer -r <readers> -w <writers>");
     }
     else{
         nb_philosophes = atoi(argv[2]);
         if(nb_philosophes < 1) return EXIT_FAILURE;
-        if(philosophe == 1) return EXIT_SUCCES;
+        if(nb_philosophes == 1) return EXIT_SUCCESS;
         printf("Starting program with %d philosophers...", nb_philosophes);
     }
 
     
-    philosophes_t phil[nb_philosophes];
+    philosophe_t phil[nb_philosophes];
     pthread_t threads[nb_philosophes];
 
     //initiation des identifiants et des sémaphores des baguettes
@@ -97,7 +89,7 @@ int main(int argc, char *argv[])
         pthread_join(threads[i], NULL);
     }
     for(int i=0; i<nb_philosophes; i++){
-        pthread_mutexattr_destroy(&phil[i].right);
+        pthread_mutex_destroy(&phil[i].right);
     }
     exit(EXIT_SUCCESS);
 }
