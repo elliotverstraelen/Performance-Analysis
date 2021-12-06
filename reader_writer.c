d29f97162449cb17d56593c79c67dd51dc17c73b
Elliot Verstraelen <elliot.verstraelen@telenet.be>
2021-12-06 16:53:11 +0100

    corrections de main() dans reader_writer.c (init. des mutex et sem)

--- reader_writer.c
+++ reader_writer.c
@@ -6,9 +6,16 @@
 #include <stdlib.h>
 
 pthread_rwlock_t lock_rw = PTHREAD_RWLOCK_INITIALIZER;
+int read_ops = 2560;
+int write_ops = 640;
+int nb_writer, nb_reader;
+
+pthread_mutex_t w_mutex, r_mutex;
+sem_t w_sem, r_sem;
 
 int main(int argc, char* argv[])
 {
+    //Il faudrait recoder le parseur d'arguments pour que l'ordre des arguments n'aient pas d'importance
     int rd_n, wr_n;
     if(argc < 5){
         // autre poss. : lance le programme avec des valeurs par defaut
@@ -17,22 +24,28 @@ int main(int argc, char* argv[])
     else if((argv[1]!="-r") || !(argv[2].isdigit()) || (argv[3]!="-w") || !(argv[4].isdigit()) ){
         printf("Erreur dans les arguments. Veuillez préciser un nombre de readers et writers.\n ./reader_writer -r <readers> -w <writers>");
     }
-    else{
+    else
+    {
         rd_n = argv[2];
         wr_n = argv[4];
         printf("Starting program with %d readers and %d writers...", rd_n, wr_n);
     }
+    pthread_mutex_init(&r_mutex, NULL);
+    pthread_mutex_init(&w_mutex, NULL); //Initialisation des mutex
+    sem_init(&r_sem, 0, 1);
+    sem_init(&w_sem, 0, 1);
+
     void *vp;
     //creation des threads readers et writers
     pthread_t readers[rd_n], writers[wr_n];
     for(int i=0; i<rd_n; i++){
-        if((pthread_create(*readers[i], NULL, *reader, &lock_rw) != 0){ //probleme de pointuer ?
+        if((pthread_create(readers[i], NULL, reader, NULL) != 0){ //probleme de pointuer ?
             perror("erreur de creation du thread reader");
             exit(-1);
         }
     }
     for(int i=0; i<wr_n; i++){
-        if((pthread_create(*writers[i], NULL, *writer, &lock_rw) != 0){ //probleme de pointuer ?
+        if((pthread_create(writers[i], NULL, writer, NULL) != 0){ //probleme de pointuer ?
             perror("erreur de creation du thread writer");
             exit(-1);
         }
@@ -49,6 +62,11 @@ int main(int argc, char* argv[])
             exit(-1);
         }
     }
+    pthread_mutex_destroy(&w_mutex);
+    pthread_mutex_destroy(r_mutex);
+    sem_destroy(&w_sem);
+    sem_destroy(&r_sem);
+
     return 0
 }
 void *reader(void *rd_n){
