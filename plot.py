b78903827849f455886884fb0583282bb541a362
Jean-Baptiste Cayman <jbcayman@MacBook-Pro-de-Jean-Baptiste-2.local>
2021-12-07 22:38:51 +0100

    plot.py test

--- plot.py
+++ plot.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
-# Exemple d'utilisation de la librairie python matplotlib pour LINFO1252
-
-# On importe numpy (pour se faciliter la vie) et matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas
 
-csv = pandas.read_csv("../csv/philo.csv")
+csv = pandas.read_csv("csv/philo.csv")
 print(csv)
 
-# moyenne
+# mean
 mean = csv.groupby(["thread"]).mean()["time"]
 std = csv.groupby(["thread"]).std()["time"]
 threads = csv["thread"].unique()
@@ -35,5 +32,5 @@ plt.savefig("philo_graph.png")
 
 plt.show()
 
-# On ferme proprement le plot.
+# Closing the plot.
 plt.close()
